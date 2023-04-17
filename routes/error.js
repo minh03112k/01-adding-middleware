0a1b072eb029b6b73f73f5dd9b39509a15f85d21
minhnguyen4 <hoangminh03112k@gmail.com>
2023-04-17 07:07:27 +0700

    passing data with post request

--- routes/error.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-router.use('/', (req, res, next) => {
-  res.status(404).send('<h1>404 Error!!!!</h1>');
-});
-
-module.exports = router;
